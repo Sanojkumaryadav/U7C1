042b28d8e16f02e354ce2239f1908b1a0925c2f7
sanojkumaryadav <sanojsanoj638@gmail.com>
2022-07-18 22:01:03 +0530

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.post('/getmeip', getMeIp);
+const dns = require("dns");
+
+function getMeIp(req, res) {
+    const body = req.body;
+    const url = body.website_name;
+    dns.resolve4(url, (err, addresses) => {
+        if (err) {
+            console.err(err);
+            return;
+        }
+        res.status(200).send(addresses[0])
+    });   
+}
+app.listen(7000, () => {
+    console.log('Server is running on port 7000');
+})
